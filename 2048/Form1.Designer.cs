045f3774273ee6c6d3348a950c16a429770ac960
Sara <sara.lazarevska24@gmail.com>
2024-07-05 16:09:23 +0200

    Commit message

---
+++ 2048/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(533, 340);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonUp.Location = new System.Drawing.Point(250, 414);
+            this.buttonUp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(49, 32);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonDown.Location = new System.Drawing.Point(250, 450);
+            this.buttonDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(49, 32);
+            this.buttonDown.TabIndex = 2;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonLeft.Location = new System.Drawing.Point(202, 450);
+            this.buttonLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(44, 32);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonRight.Location = new System.Drawing.Point(303, 450);
+            this.buttonRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(49, 32);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // labelScore
+            // 
+            this.labelScore.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelScore.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.Location = new System.Drawing.Point(0, 367);
+            this.labelScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(533, 26);
+            this.labelScore.TabIndex = 5;
+            this.labelScore.Text = "Score: 0";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 520);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "2048 Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
---
+++ 2048/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        private const int GridSize = 4;
+        private Button[,] grid;
+        private Random random = new Random();
+        private int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGrid();
+            StartNewGame();
+        }
+
+        private void InitializeGrid()
+        {
+            grid = new Button[GridSize, GridSize];
+
+            for (int i = 0; i < GridSize; i++)
+            {
+                for (int j = 0; j < GridSize; j++)
+                {
+                    var button = new Button
+                    {
+                        Dock = DockStyle.Fill,
+                        Font = new Font("Arial", 24, FontStyle.Bold),
+                        BackColor = Color.LightGray
+                    };
+                    tableLayoutPanel1.Controls.Add(button, j, i);
+                    grid[i, j] = button;
+                }
+            }
+
+            KeyPreview = true;
+            KeyDown += Form1_KeyDown;
+        }
+
+        private void StartNewGame()
+        {
+            foreach (var button in grid)
+            {
+                button.Text = "";
+                button.BackColor = Color.LightGray;
+            }
+
+            AddRandomTile();
+            AddRandomTile();
+            score = 0;
+            UpdateScore();
+        }
+
+        private void AddRandomTile()
+        {
+            var emptyTiles = new List<Button>();
+            foreach (var button in grid)
+            {
+                if (button.Text == "")
+                {
+                    emptyTiles.Add(button);
+                }
+            }
+
+            if (emptyTiles.Count > 0)
+            {
+                var tile = emptyTiles[random.Next(emptyTiles.Count)];
+                tile.Text = random.Next(0, 2) == 0 ? "2" : "4";
+                UpdateTileColor(tile);
+            }
+        }
+
+        private void UpdateTileColor(Button tile)
+        {
+            switch (tile.Text)
+            {
+                case "2":
+                    tile.BackColor = Color.Beige;
+                    break;
+                case "4":
+                    tile.BackColor = Color.Bisque;
+                    break;
+                case "8":
+                    tile.BackColor = Color.BurlyWood;
+                    break;
+                case "16":
+                    tile.BackColor = Color.Coral;
+                    break;
+                case "32":
+                    tile.BackColor = Color.DarkOrange;
+                    break;
+                case "64":
+                    tile.BackColor = Color.OrangeRed;
+                    break;
+                case "128":
+                    tile.BackColor = Color.Gold;
+                    break;
+                case "256":
+                    tile.BackColor = Color.Goldenrod;
+                    break;
+                case "512":
+                    tile.BackColor = Color.Khaki;
+                    break;
+                case "1024":
+                    tile.BackColor = Color.Yellow;
+                    break;
+                case "2048":
+                    tile.BackColor = Color.YellowGreen;
+                    break;
+                default:
+                    tile.BackColor = Color.LightGray;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool moved = false;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    moved = MoveUp();
+                    break;
+                case Keys.Down:
+                    moved = MoveDown();
+                    break;
+                case Keys.Left:
+                    moved = MoveLeft();
+                    break;
+                case Keys.Right:
+                    moved = MoveRight();
+                    break;
+            }
+
+            if (moved)
+            {
+                AddRandomTile();
+                if (IsGameOver())
+                {
+                    MessageBox.Show("Game Over!");
+                }
+            }
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            if (MoveUp())
+            {
+                AddRandomTile();
+                if (IsGameOver())
+                {
+                    MessageBox.Show("Game Over!");
+                }
+            }
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            if (MoveDown())
+            {
+                AddRandomTile();
+                if (IsGameOver())
+                {
+                    MessageBox.Show("Game Over!");
+                }
+            }
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            if (MoveLeft())
+            {
+                AddRandomTile();
+                if (IsGameOver())
+                {
+                    MessageBox.Show("Game Over!");
+                }
+            }
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            if (MoveRight())
+            {
+                AddRandomTile();
+                if (IsGameOver())
+                {
+                    MessageBox.Show("Game Over!");
+                }
+            }
+        }
+
+        private bool MoveUp()
+        {
+            bool moved = false;
+            for (int col = 0; col < GridSize; col++)
+            {
+                for (int row = 1; row < GridSize; row++)
+                {
+                    if (grid[row, col].Text != "")
+                    {
+                        int targetRow = row;
+                        while (targetRow > 0 && grid[targetRow - 1, col].Text == "")
+                        {
+                            targetRow--;
+                        }
+                        if (targetRow > 0 && grid[targetRow - 1, col].Text == grid[row, col].Text)
+                        {
+                            grid[targetRow - 1, col].Text = (int.Parse(grid[targetRow - 1, col].Text) * 2).ToString();
+                            grid[row, col].Text = "";
+                            moved = true;
+                            score += int.Parse(grid[targetRow - 1, col].Text);
+                        }
+                        else if (targetRow != row && grid[targetRow, col].Text == "")
+                        {
+                            grid[targetRow, col].Text = grid[row, col].Text;
+                            grid[row, col].Text = "";
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            UpdateAllTileColors();
+            UpdateScore();
+            return moved;
+        }
+
+        private bool MoveDown()
+        {
+            bool moved = false;
+            for (int col = 0; col < GridSize; col++)
+            {
+                for (int row = GridSize - 2; row >= 0; row--)
+                {
+                    if (grid[row, col].Text != "")
+                    {
+                        int targetRow = row;
+                        while (targetRow < GridSize - 1 && grid[targetRow + 1, col].Text == "")
+                        {
+                            targetRow++;
+                        }
+                        if (targetRow < GridSize - 1 && grid[targetRow + 1, col].Text == grid[row, col].Text)
+                        {
+                            grid[targetRow + 1, col].Text = (int.Parse(grid[targetRow + 1, col].Text) * 2).ToString();
+                            grid[row, col].Text = "";
+                            moved = true;
+                            score += int.Parse(grid[targetRow + 1, col].Text);
+                        }
+                        else if (targetRow != row && grid[targetRow, col].Text == "")
+                        {
+                            grid[targetRow, col].Text = grid[row, col].Text;
+                            grid[row, col].Text = "";
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            UpdateAllTileColors();
+            UpdateScore();
+            return moved;
+        }
+
+        private bool MoveLeft()
+        {
+            bool moved = false;
+            for (int row = 0; row < GridSize; row++)
+            {
+                for (int col = 1; col < GridSize; col++)
+                {
+                    if (grid[row, col].Text != "")
+                    {
+                        int targetCol = col;
+                        while (targetCol > 0 && grid[row, targetCol - 1].Text == "")
+                        {
+                            targetCol--;
+                        }
+                        if (targetCol > 0 && grid[row, targetCol - 1].Text == grid[row, col].Text)
+                        {
+                            grid[row, targetCol - 1].Text = (int.Parse(grid[row, targetCol - 1].Text) * 2).ToString();
+                            grid[row, col].Text = "";
+                            moved = true;
+                            score += int.Parse(grid[row, targetCol - 1].Text);
+                        }
+                        else if (targetCol != col && grid[row, targetCol].Text == "")
+                        {
+                            grid[row, targetCol].Text = grid[row, col].Text;
+                            grid[row, col].Text = "";
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            UpdateAllTileColors();
+            UpdateScore();
+            return moved;
+        }
+
+        private bool MoveRight()
+        {
+            bool moved = false;
+            for (int row = 0; row < GridSize; row++)
+            {
+                for (int col = GridSize - 2; col >= 0; col--)
+                {
+                    if (grid[row, col].Text != "")
+                    {
+                        int targetCol = col;
+                        while (targetCol < GridSize - 1 && grid[row, targetCol + 1].Text == "")
+                        {
+                            targetCol++;
+                        }
+                        if (targetCol < GridSize - 1 && grid[row, targetCol + 1].Text == grid[row, col].Text)
+                        {
+                            grid[row, targetCol + 1].Text = (int.Parse(grid[row, targetCol + 1].Text) * 2).ToString();
+                            grid[row, col].Text = "";
+                            moved = true;
+                            score += int.Parse(grid[row, targetCol + 1].Text);
+                        }
+                        else if (targetCol != col && grid[row, targetCol].Text == "")
+                        {
+                            grid[row, targetCol].Text = grid[row, col].Text;
+                            grid[row, col].Text = "";
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            UpdateAllTileColors();
+            UpdateScore();
+            return moved;
+        }
+
+        private void UpdateAllTileColors()
+        {
+            foreach (var tile in grid)
+            {
+                UpdateTileColor(tile);
+            }
+        }
+
+        private void UpdateScore()
+        {
+            labelScore.Text = $"Score: {score}";
+        }
+
+        private bool IsGameOver()
+        {
+            foreach (var button in grid)
+            {
+                if (button.Text == "")
+                {
+                    return false;
+                }
+            }
+
+            for (int row = 0; row < GridSize; row++)
+            {
+                for (int col = 0; col < GridSize; col++)
+                {
+                    if (row < GridSize - 1 && grid[row, col].Text == grid[row + 1, col].Text)
+                    {
+                        return false;
+                    }
+                    if (col < GridSize - 1 && grid[row, col].Text == grid[row, col + 1].Text)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
